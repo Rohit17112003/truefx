86fb6f61fc26faa2cef5259ce521c8d6d0507658
Rohit17112003 <133210462+Rohit17112003@users.noreply.github.com>
2025-06-05 16:28:48 +0530

    some change in header

--- components/organisms/Header.jsx
+++ components/organisms/Header.jsx
@@ -1,7 +1,8 @@
 "use client";
 import Link from "next/link";
 import { useTheme } from "next-themes";
-import { FaMoon, FaSun } from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
+import { IoSunnyOutline } from "react-icons/io5";
 
 const Header = ({ isSidebarOpen, setIsSidebarOpen, pageTitle }) => {
   const { theme, setTheme } = useTheme(); // 🔧 Added setTheme
@@ -11,7 +12,7 @@ const Header = ({ isSidebarOpen, setIsSidebarOpen, pageTitle }) => {
       <div className="flex items-center gap-4 sm:gap-0">
         <button
           onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-          className="text-[1.3rem] outline-none md:hidden"
+          className="text-[1.3rem] outline-none md:hidden text-black dark:text-white"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -43,18 +44,15 @@ const Header = ({ isSidebarOpen, setIsSidebarOpen, pageTitle }) => {
             onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
           >
             {theme === "dark" ? (
-              <FaMoon className="h-5 w-5 text-black md:h-6 md:w-6" />
+              <FaMoon className="h-5 w-5 text-black dark:text-white md:h-6 md:w-6" />
             ) : (
-              <img
-                src="/image/Frame.svg"
-                alt="user"
-                className="h-7 w-7 md:h-10 md:w-10"
-              />
+              <IoSunnyOutline className="h-7 w-7 text-black dark:text-white md:h-8 md:w-8" />
+             
             )}
           </span>
 
           {/* 🔔 Notification */}
-          <span className="mt-3 text-[1.2rem] md:text-[1.4rem]">
+          <span className="mt-3 text-[1.2rem] md:text-[1.4rem] text-black dark:text-white">
             <i className="ri-notification-2-fill relative">
               <p className="mukta-extralight absolute -mt-8.5 ml-2.5 h-4.5 w-4.5 items-center rounded-full bg-[#c52323] text-center text-[0.81rem] text-white md:-mt-10 md:ml-3 md:h-5.5 md:w-5.5 md:text-[0.9rem]">
                 2
@@ -65,7 +63,7 @@ const Header = ({ isSidebarOpen, setIsSidebarOpen, pageTitle }) => {
           {/* 👤 Profile */}
 
           <div className="flex aspect-square h-12 w-28 items-center justify-center gap-2">
-            <h1 className="text-center text-[0.8rem] leading-4 font-bold md:leading-5 dark:text-white">
+            <h1 className="text-center text-[0.8rem] leading-4 font-bold md:leading-5 dark:text-white text-black">
               Mr Rohit Rana
             </h1>
             <Link href="/profile">
